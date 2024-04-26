f460b1be99798f603c52d4e11c0043818cbac88b
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-04-26 15:08:42 +0500

    cors and cookie middlewares settings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 dotenv.config({
-    path : './env'
+    path: './env'
 })
 
 
 connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Error in server", err);
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+
+    .catch((err) => {
+        console.log("Mongo DB Connection Failed", err);
+    })
