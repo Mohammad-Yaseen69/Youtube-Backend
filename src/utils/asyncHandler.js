ad744a32b29b89c1907a627301976a801ca60f11
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-05-01 15:46:03 +0500

    fix - typo

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (fn) => async (req , res , next) => {
     try {
         await fn(req, res, next)
     } catch (error) {
-        req.status(error.code || 500).json({
+        res.status(error.code || 500).json({
             message: error.message,
             success: false
         })
