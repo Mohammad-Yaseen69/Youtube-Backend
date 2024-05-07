38d182655853f9bea84cd0c734508636690edece
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-05-07 16:20:11 +0500

    routes for updating user details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAvatar,
+    updateUserDetails,
+    updateCoverImg
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +36,10 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/get-current-user").get(verifyJWT,getCurrentUser)
+router.route("/get-current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").post(verifyJWT, updateUserDetails)
+router.route("/updateAvatar").post(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/updateCoverImg").post(verifyJWT, upload.single("coverImg"), updateCoverImg)
+
 
 export default router;
