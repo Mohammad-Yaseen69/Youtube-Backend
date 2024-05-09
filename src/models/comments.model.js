4cfc84efaf80d0b63c9ee932369366dc563610d7
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-05-09 17:02:19 +0500

    Add - comment, likes, playlist and tweet models

---
+++ src/models/comments.model.js
@@ -0,0 +1,22 @@
+import mongoose,{Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+
+const commentsSchema = new Schema({
+    content : {
+        type : String,
+        required : true
+    },
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    },
+    video : {
+        type : Schema.Types.ObjectId,
+        ref : "Video"
+    }
+} , {timestamps : true})
+
+commentsSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = mongoose.model("Comment" , commentsSchema)
